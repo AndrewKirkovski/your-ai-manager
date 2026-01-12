f62eb682db13fb6382166439182058ad1af8f54f
Andrei Kirkouski <andrew@spiralscout.com>
2026-01-12 19:02:18 +0100

    fix web ui

--- webServer.ts
+++ webServer.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { getAllUsers, getUser, updateUserTask, updateUserMemory, setUser } from './userStore';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
